4469f7922fc4af7cee08f1f4c21b52597f768b83
Adam Harrison <ad@mharrison.us>
2018-01-15 21:21:12 -0500

    Copy from SCO, replacing gson with jackson

---
+++ src/main/java/com/ulfric/spring/jclouds/blobstore/BlobRepository.java
@@ -0,0 +1,9 @@
+package com.ulfric.spring.jclouds.blobstore;
+
+public interface BlobRepository {
+
+	void put(String key, Object value);
+
+	<T> T get(String key, Class<T> type);
+
+}
---
+++ src/main/java/com/ulfric/spring/jclouds/blobstore/impl/BlobRepositoryImpl.java
@@ -0,0 +1,81 @@
+package com.ulfric.spring.jclouds.blobstore.impl;
+
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.jclouds.ContextBuilder;
+import org.jclouds.blobstore.BlobStore;
+import org.jclouds.blobstore.BlobStoreContext;
+import org.jclouds.blobstore.domain.Blob;
+import org.jclouds.io.Payload;
+import org.springframework.beans.factory.annotation.Value;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+import com.ulfric.spring.jclouds.blobstore.BlobRepository;
+
+public class BlobRepositoryImpl implements BlobRepository {
+
+	@Inject
+	private ObjectMapper json;
+
+	@Value("${BUCKET_NAME:springjclouds}")
+	private String containerName;
+
+	@Value("${JCLOUDS_PROVIDER:transient}")
+	private String jcloudsProvider;
+
+	@Value("${JCLOUDS_ID:none}")
+	private String jcloudsId;
+
+	@Value("${JCLOUDS_SECRET:}")
+	private String jcloudsSecret;
+
+	private BlobStoreContext context;
+
+	@PostConstruct
+	public void createBucket() {
+		context = ContextBuilder.newBuilder(jcloudsProvider)
+				.credentials(jcloudsId, jcloudsSecret.replace("\\n", "\n"))
+				.build(BlobStoreContext.class);
+
+		getBlobStore().createContainerInLocation(null, containerName);
+	}
+
+	@Override
+	public void put(String key, Object value) {
+		BlobStore blobStore = getBlobStore();
+		Blob blob;
+		try {
+			blob = blobStore.blobBuilder(key).payload(json.writeValueAsString(value)).build();
+		} catch (JsonProcessingException exception) {
+			throw new RuntimeException("Could not parse json", exception);
+		}
+		blobStore.putBlob(containerName, blob);
+	}
+
+	@Override
+	public <T> T get(String key, Class<T> type) {
+		Blob blob = getBlobStore().getBlob(containerName, key);
+		if (blob == null) {
+			return null;
+		}
+
+		Payload payload = blob.getPayload();
+		try {
+			return payload == null ? null : json.readValue(payload.openStream(), type);
+		} catch (JsonSyntaxException | JsonIOException | IOException exception) {
+			exception.printStackTrace(); // TODO proper error handling
+			return null;
+		}
+	}
+
+	public BlobStore getBlobStore() {
+		return context.getBlobStore();
+	}
+
+}
